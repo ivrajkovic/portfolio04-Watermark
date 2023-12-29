26d3a5623fb045d703cb873f7d4efee6787d2341
Ivan Rajković <124137529+ivrajkovic@users.noreply.github.com>
2023-12-29 13:01:00 +0100

    Add watermark function
    Functions: check watermark text legth, image loaded, image format, reset.
    Minor code adjustments: define, browse, remaming.

--- main.py
+++ main.py
@@ -1,38 +1,96 @@
 import tkinter
 from tkinter import filedialog, messagebox
 import os
-from PIL import Image, ImageFont, ImageFont
+from PIL import Image, ImageFont, ImageDraw
 
-# Define
+# Define startup variables
+#   Colors
 color_bg = "#F5F7F8"
 color_fg = "#45474B"
 color_tint1 = "#F4CE14"
 color_tint2 = "#495E57"
+#  Globals
 filename = ''
 
 
 # Open file explorer window
-def browse():
+def watermark_browse():
     global filename
     filetypes = (
         ("jpeg", "*.jpeg"),
         ("jpg", "*.jpg"),
         ("png", "*.png"),
-        ('All files', '*.*')
+        ('all files', '*.*')
     )
     filename = filedialog.askopenfilename(
         initialdir="/",
         title="Select a File",
-        filetypes=filetypes)
-    label_open_file_location.configure(text=os.path.basename(filename))
+        filetypes=filetypes
+    )
+    if filename and watermark_image_format(filename):
+        label_open_file_location.configure(text=os.path.basename(filename))
 
 
-# Add watermark
+# Check watermark text length
+def watermark_text_entered():
+    text = input_watermark.get()
+    if len(text) < 3 or len(text) > 10:
+        messagebox.showinfo("Watermark Error", "Watermark text must be between 3 and 10 characters")
+        return False
+    else:
+        return True
+
+
+# Check if watermark file is loaded
+def watermark_image_loaded(file):
+    if not file:
+        messagebox.showinfo("Image Error", "Image file not provided.")
+        return False
+    else:
+        return True
+
+
+# Check if watermark file is in proper format
+def watermark_image_format(file):
+    try:
+        image = Image.open(file)
+        image.close()
+    except IOError:
+        messagebox.showinfo("Image Error", "Provide proper image file.")
+        return False
+    else:
+        return True
+
+
+# Reset filename, watermark text
+def reset_all():
+    global filename
+    filename = ""
+    label_open_file_location.configure(text="..select image..")
+    input_watermark.delete(0, tkinter.END)
+
+
+# Add watermark to image
+def watermark():
+    if watermark_image_loaded(filename) and watermark_image_format(filename) and watermark_text_entered():
+        image = Image.open(filename)
+        width, height = image.size
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype('arial.ttf', 30)
+        fill_color = (255, 255, 255)
+        watermark_text = input_watermark.get()
+        x = width/2 - 30
+        y = height/2 - 30
+        position = (x, y)
+        draw.text(xy=position, text=watermark_text, font=font, fill=fill_color)
+        image.save(os.path.dirname(filename) + "/wm_" + os.path.basename(filename))
+        messagebox.showinfo("Success", "Watermark added successfully.")
+        reset_all()
+
 
 # Window config
 window = tkinter.Tk()
 window.title("Watermark project")
-# window.geometry("400x250")
 window.config(padx=20, pady=20, bg=color_bg)
 
 # Labels
@@ -46,8 +104,8 @@ label_watermark = tkinter.Label(text="Watermark text:", font=("Arial", 12), pady
 input_watermark = tkinter.Entry(font=("Arial", 12, "bold"), width=18, bg=color_tint1, fg=color_tint2)
 
 # Buttons
-button_browse_file = tkinter.Button(text="Browse", font=("Arial", 12), width=17, bg=color_tint1, command=browse)
-button_watermark = tkinter.Button(text="Place watermark", font=("Arial", 12), width=17, bg=color_tint1, command='')
+button_browse_file = tkinter.Button(text="Browse", font=("Arial", 12), width=17, bg=color_tint1, command=watermark_browse)
+button_watermark = tkinter.Button(text="Place watermark", font=("Arial", 12), width=17, bg=color_tint1, command=watermark)
 button_exit = tkinter.Button(text="Exit", font=("Arial", 12), width=17, bg=color_tint2, fg=color_bg, command=exit)
 
 # Grid setup
@@ -66,5 +124,6 @@ input_watermark.grid(row=3, column=1)
 button_watermark.grid(row=4, column=0)
 #   Row 5
 button_exit.grid(row=4, column=1)
+
 # Stay on screen
 window.mainloop()
